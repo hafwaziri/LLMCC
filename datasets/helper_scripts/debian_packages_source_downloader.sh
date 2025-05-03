e494460d10eeda1a134e6116f6aabf009a0832db
Hafiz Waziri <hafizullah.waziri@auaf.edu.af>
2025-05-03 18:37:36 +0200

    Bash script to download source files for the debian packages

---
+++ datasets/helper_scripts/debian_packages_source_downloader.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+check_install_package() {
+    local package_name="$1"
+
+    if ! dpkg -l "$package_name" | grep -q "^ii"; then
+        echo "Package $package_name is not installed. Installing..."
+
+        sudo apt update
+        sudo apt install debmirror
+
+        if ! dpkg -l "$package_name" | grep -q "^ii"; then
+            echo "Failed to install $package_name. Exiting."
+            exit 1
+        else
+            echo "$package_name successfully installed."
+        fi
+    else
+        echo "Package $package_name is already installed."
+    fi
+}
+
+download_source() {
+
+    local dest_dir="../debian-sources"
+    local mirror="deb.debian.org/debian"
+    local architecture="source"
+    local section="main,contrib,non-free"
+    local release="stable"
+
+    debmirror \
+        --progress \
+        --verbose \
+        --method=http \
+        --host="$mirror" \
+        --root="/" \
+        --arch="$architecture" \
+        --dist="$release" \
+        --section="$section" \
+        --no-check-gpg \
+        --ignore-release-gpg \
+        --source \
+        "$dest_dir"
+
+    echo "-------------------------------------"
+    echo "Download complete!"
+    echo "-------------------------------------"
+
+}
+
+check_install_package "debmirror"
+download_source
