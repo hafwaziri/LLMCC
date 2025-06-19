ac8fb38618e8e2e0fc8bf07cc26b1c98d6e9466a
Hafiz Waziri <hafizullah.waziri@auaf.edu.af>
2025-06-19 19:04:29 +0200

    fix: correct paths to copy packages from and to in cp_random_packages.sh

--- datasets/helper_scripts/cp_random_packages.sh
+++ datasets/helper_scripts/cp_random_packages.sh
@@ -2,9 +2,12 @@
 
 NUM_PACKAGES=$1
 
-cd ../../debian-source
+SOURCE_DIR="../../../../debian-source" 
+DEST_DIR="../pilot/LLMCC/datasets/debian-source"
+
+cd $SOURCE_DIR
 PACKAGES=$(ls | shuf -n $NUM_PACKAGES)
 
 for pkg in $PACKAGES; do
-  cp -r "$pkg" "../pilot/debian-source"
+  cp -r "$pkg" $DEST_DIR
 done
