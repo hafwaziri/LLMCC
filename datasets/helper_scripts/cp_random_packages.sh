aa7f17da1caccffb04a9d0d4e1fdde5e938d021f
Hafiz Waziri <hafizullah.waziri@auaf.edu.af>
2025-07-27 13:59:02 +0200

    feat(cp_random_packages): ability to clean the debian-sources directory

--- datasets/helper_scripts/cp_random_packages.sh
+++ datasets/helper_scripts/cp_random_packages.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-NUM_PACKAGES=$1
+#USAGE: ./cp_random_packages.sh [--clean] [NUM_PACKAGES] [SPECIFIC_PACKAGE]
 
-SOURCE_DIR="../../../../debian-source" 
+SOURCE_DIR="../../../../debian-source"
 DEST_DIR="../pilot/LLMCC/datasets/debian-source"
+cd "$SOURCE_DIR"
 
-cd $SOURCE_DIR
-PACKAGES=$(ls | shuf -n $NUM_PACKAGES)
+if [ ! -d "$DEST_DIR" ]; then
+  mkdir "$DEST_DIR"
+fi
 
-for pkg in $PACKAGES; do
-  cp -r "$pkg" $DEST_DIR
-done
+if [ "$1" = "--clean" ]; then
+  rm -rf "${DEST_DIR:?}/"*
+  shift # Remove --clean from arguments
+fi
+
+if [ -n "$2" ]; then
+  cp -r "$2" "$DEST_DIR"
+else
+  NUM_PACKAGES=$1
+  if [ -n "$NUM_PACKAGES" ]; then
+    PACKAGES=$(ls | shuf -n "$NUM_PACKAGES")
+    
+    for pkg in $PACKAGES; do
+      cp -r "$pkg" "$DEST_DIR"
+    done
+  fi
+fi
