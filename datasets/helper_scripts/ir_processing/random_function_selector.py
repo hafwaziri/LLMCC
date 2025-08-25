1c170365c11460de1920b264a7e7df4896da9835
Hafiz Waziri <hafizullah.waziri@auaf.edu.af>
2025-08-25 09:52:06 +0200

    documentation(random_function_selector): source file documented

--- datasets/helper_scripts/ir_processing/random_function_selector.py
+++ datasets/helper_scripts/ir_processing/random_function_selector.py
@@ -1,11 +1,14 @@
 """
-random_function_selector.py - 
+random_function_selector.py - Select a random function from a list with reproducibility
 
 Description:
-
+    This module provides a utility to select a random function from a given list of functions.
+    The selection can be made reproducible by specifying a random seed.
 
 Functions:
-
+    random_function_selector(functions, random_seed=None)
+        Selects and returns a random function from the provided list.
+        If random_seed is specified, the selection is reproducible.
 """
 
 import random
